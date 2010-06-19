bb0ba84704d94c3362ce4683a12022f9426cbc1a
Daniel Cohen <danielc192@gmail.com>
2010-06-19 17:28:41 -0400

    Converted stat_cdf so it calls necessary functions by itself

--- statistics.c
+++ statistics.c
@@ -13,17 +13,23 @@
 #include "ruby/config.h"
 #include <math.h>
 
-struct stat_stdev_args {
+struct stat_data_args {
    double sum, sum2;
    long num;
 };
 
 
+double dblmod(double a, double b)
+{
+int result = (int) ( a / b );
+return a - (double) ( result ) * b;
+}
+
 
 static VALUE
-stdev_i(VALUE elem, VALUE arg, int argc, VALUE *argv)
+stat_i(VALUE elem, VALUE arg, int argc, VALUE *argv)
 {
-   struct stat_stdev_args *a = (void *)arg;
+   struct stat_data_args *a = (void *)arg;
    double x = NUM2DBL(elem);
    a->sum += x;
    a->sum2 += pow(x, 2);
@@ -43,12 +49,12 @@ static VALUE
 stat_stddev(VALUE obj, VALUE list)
 {
    long i;
-   struct stat_stdev_args args;
+   struct stat_data_args args;
    double mean, stddev;
 
    args.sum = args.sum2 = 0.0;
    args.num = 0;
-   rb_iterate(rb_each, list, stdev_i, (VALUE)&args);
+   rb_iterate(rb_each, list, stat_i, (VALUE)&args);
    mean = args.sum / args.num;
    stddev = sqrt(args.sum2 / args.num - mean*mean);
    return rb_float_new(stddev);
@@ -66,11 +72,11 @@ stat_stddev(VALUE obj, VALUE list)
 static VALUE
 stat_sum(VALUE obj, VALUE list)
 {
-   struct stat_stdev_args args;
+   struct stat_data_args args;
    
    args.sum = args.sum2 = 0.0;
    args.num = 0;
-   rb_iterate(rb_each, list, stdev_i, (VALUE)&args);
+   rb_iterate(rb_each, list, stat_i, (VALUE)&args);
    return rb_float_new(args.sum);
 }
 
@@ -98,11 +104,11 @@ stat_sum(VALUE obj, VALUE ary)
 static VALUE
 stat_sum2(VALUE obj, VALUE list)
 {
-   struct stat_stdev_args args;
+   struct stat_data_args args;
 
    args.sum = args.sum2 = 0.0;
    args.num = 0;
-   rb_iterate(rb_each, list, stdev_i, (VALUE)&args);
+   rb_iterate(rb_each, list, stat_i, (VALUE)&args);
    return rb_float_new(args.sum2);
 }
 
@@ -117,12 +123,12 @@ stat_sum2(VALUE obj, VALUE list)
 static VALUE
 stat_stddev_s(VALUE obj, VALUE list)
 {
-	struct stat_stdev_args args;
+	struct stat_data_args args;
 	double mean, stddevs;
 	
 	args.sum = args.sum2 = 0.0;
 	args.num = 0;
-	rb_iterate(rb_each, list, stdev_i, (VALUE)&args);
+	rb_iterate(rb_each, list, stat_i, (VALUE)&args);
 	mean = args.sum / args.num;
 	stddevs = sqrt((args.sum2 - (pow(args.sum,2) / args.num)) / (args.num - 1)); 
 	return rb_float_new(stddevs);
@@ -139,12 +145,12 @@ stat_stddev_s(VALUE obj, VALUE list)
 static VALUE
 stat_mean(VALUE obj, VALUE list)
 {
-	struct stat_stdev_args args;
-	double mean, stddevs;
+	struct stat_data_args args;
+	double mean;
 	
 	args.sum = args.sum2 = 0.0;
 	args.num = 0;
-	rb_iterate(rb_each, list, stdev_i, (VALUE)&args);
+	rb_iterate(rb_each, list, stat_i, (VALUE)&args);
 	mean = args.sum / (double) args.num;
 	return rb_float_new(mean);
 }
@@ -155,16 +161,16 @@ stat_mean(VALUE obj, VALUE list)
  *
  *  Computes the Area under a Cumulative Distribution or Normal Distribution
  *  given the <i>standard deviation</i>, <i>mean</i> and <i>value</i> to compute 
- *  the area to the left of
+ *  the area to the left of x
  *  If they are all numbers.
  */
 
 static VALUE
-stat_cdf(VALUE obj, VALUE sig, VALUE mu, VALUE x)
+stat_cdf(VALUE obj, VALUE list)
 {
 	double numer, denom;
-	numer = x - mu;
-	denom = sig * sqrt(2);
+	numer = x - stat_mean(0,list);
+	denom = stat_stddev(0,list) * sqrt(2);
 	return rb_float_new(0.5+0.5*erf(numer/denom));
 }
 
@@ -172,23 +178,20 @@ stat_cdf(VALUE obj, VALUE sig, VALUE mu, VALUE x)
 /*static VALUE
 stat_median(VALUE obj, VALUE list)
 {
-	int i;
-	for(i = 0;i<RARRAY_LEN(list);i++)
+	struct stat_data_args args;
+	double median, middle;
+	
+    middle = NUM2DBL(RARRAY_LEN(list))/ 2;
+
+	if ((dblmod(middle,2)) == 0)
 	{
-		if ( i == (RARRAY_LEN(list) / 2)) {
-			if((RARRAY_LEN(list) % 2) == 0) {
-				return rb_float_new((NUM2DBL(RARRAY_PTR(list)[i]) + NUM2DBL(RARRAY_PTR(list)[i-1])) / 2);
-			}
-		}
 		
-		if (i == ((RARRAY_LEN(list) / 2) - 0.5)) {
-			if((RARRAY_LEN(list) % 2) != 0) {
-				return RARRAY_PTR(list)[i];
-			}
-		}
-		
-	}	
-		
+		return rb_float_new(median);
+	}
+	else 
+	{
+	return rb_float_new(NUM2DBL(RARRAY_PTR(list)[(int) (middle + 0.5)]));
+	}
 }*/
 
 void
@@ -200,5 +203,6 @@ Init_statistics(void)
    rb_define_module_function(mStatistics, "sum2", stat_sum2, 1);
    rb_define_module_function(mStatistics, "stddevs", stat_stddev_s, 1);
    rb_define_module_function(mStatistics, "mean", stat_mean, 1);
-   rb_define_module_function(mStatistics, "cdf", stat_cdf, 3);
+   //rb_define_module_function(mStatistics, "median", stat_median, 1);
+   rb_define_module_function(mStatistics, "cdf", stat_cdf, 1);
 }
